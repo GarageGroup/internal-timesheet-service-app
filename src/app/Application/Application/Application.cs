f5ac1fcc528f192e00a72228e27eec93cd44b02c
Егор Сухоруков <SEM200107@mail.ru>
2025-10-23 18:39:00 +0300

    add logger

--- src/app/Application/Application/Application.cs
+++ src/app/Application/Application/Application.cs
@@ -12,7 +12,7 @@ internal static partial class Application
     private static Dependency<IDataverseApiClient> UseDataverseApi()
         =>
         PrimaryHandler.UseStandardSocketsHttpHandler()
-        .UseLogging("DataverseApi")
+        .UseLogging("DataverseApi", HttpLoggerType.ResponseBody)
         .UseTokenCredentialStandard()
         .UsePollyStandard()
         .UseDataverseApiClient("Dataverse");
